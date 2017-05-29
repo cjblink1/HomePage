14ab18f0759a39760d305f5100234fef14f7272c
Connor Boyle <cjblink1@gmail.com>
2017-05-28 22:59:11 -0500

    testGetAllLinksCorrectValue fails

--- test/connectors/PostgresConnector.test.ts
+++ test/connectors/PostgresConnector.test.ts
@@ -9,14 +9,14 @@ describe("Postgres Connector", () => {
     var clientMock: TypeMoq.IMock<pg.Client>;
     var postgresConnector: PostgresConnector;
 
-    beforeAll(() => {
+    beforeEach(() => {
         poolMock = TypeMoq.Mock.ofType(pg.Pool);
         clientMock = TypeMoq.Mock.ofType(pg.Client);
         poolMock.setup(x => x.connect()).returns(() => Promise.resolve(clientMock.object));
-        postgresConnector = new PostgresConnector(poolMock.object);
     });
 
     it("testInitializesClient", (done: any) => {
+        postgresConnector = new PostgresConnector(poolMock.object);
         postgresConnector.connect(() => {
             expect(postgresConnector.isConnected()).toBe(true);
             done();
@@ -24,9 +24,25 @@ describe("Postgres Connector", () => {
     });
 
     it("testGetAllLinksNotNull", (done: any) => {
+        postgresConnector = new PostgresConnector(poolMock.object);
         postgresConnector.getAllLinks((links) => {
             expect(links).not.toBeNull();
             done();
         });
-    })
+    });
+
+    it("testGetAllLinksCorrectValue", (done: any) => {
+        var queryResult: pg.QueryResult =  {
+            command: "", rowCount: 4, rows: ["Nice", "Work", "Connor"], oid: 0
+
+        }
+        clientMock.setup(x => x.query(TypeMoq.It.isValue("SELECT get_all_links(auth := $1)"),
+                                      TypeMoq.It.isValue(["validAuthParam"])))
+                                      .returns(() => Promise.resolve(queryResult));
+        postgresConnector = new PostgresConnector(poolMock.object);
+        postgresConnector.getAllLinks((links) => {
+            expect(links).toBe(["Nice", "Work", "Connor"]);
+            done();
+        });
+    });
 });
