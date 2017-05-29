5ce861eb2f752fcfb2a4b6a7e7cd0f522bc09adb
Connor Boyle <cjblink1@gmail.com>
2017-05-29 17:49:06 -0500

    testCreateLinkSuccessCase passes

--- src/connectors/PostgresConnector.ts
+++ src/connectors/PostgresConnector.ts
@@ -9,10 +9,15 @@ export class PostgresConnector {
     }
 
     public getLinksOfUser(authParam: string): Promise<any[]> {
-        return new Promise((resolve, reject) => {
+        return new Promise<any[]>((resolve, reject) => {
             this.pool.query("SELECT * FROM get_links_of_user(u_auth := $1)", [authParam])
             .then(result => resolve(result.rows), reject);
         });
+    }
+
+    public createLink(linkName: string, linkUrl: string, authParam: string): Promise<pg.QueryResult> {
+        return this.pool.query("SELECT create_link(link_name := $1, link_url := $2, u_auth := $3)",
+                               [linkName, linkUrl, authParam]);
     }
 }
 
--- test/connectors/PostgresConnector.test.ts
+++ test/connectors/PostgresConnector.test.ts
@@ -5,74 +5,109 @@ import { PostgresConnector, PostgresError } from "../../src/connectors/PostgresC
 
 describe("Postgres Connector", () => {
 
-    var poolMock: TypeMoq.IMock<pg.Pool>;
-    var postgresConnector: PostgresConnector;
+    describe("getLinksOfUser()", () => {
 
-    beforeEach(() => {
-        poolMock = TypeMoq.Mock.ofType(pg.Pool);
-    });
+        var poolMock: TypeMoq.IMock<pg.Pool>;
+        var postgresConnector: PostgresConnector;
 
-    afterEach(() => {
-        poolMock = null;
-    });
-
-    it("testLinksOfUserNotNull", (done: any) => {
-        var queryResult: pg.QueryResult =  {
-            command: "", rowCount: 3, rows: ["Nice", "Work", "Connor"], oid: 0
-        }
-        poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
-                                      TypeMoq.It.isValue(["validAuthParam"])))
-                                      .returns(() => Promise.resolve(queryResult));
-        postgresConnector = new PostgresConnector(poolMock.object);
-        postgresConnector.getLinksOfUser("validAuthParam")
-        .then(links => {
-            expect(links).not.toBeNull();
-            done();
-        }).catch(() => {
-            fail();
-            done();
+        beforeEach(() => {
+            poolMock = TypeMoq.Mock.ofType(pg.Pool);
         });
-    });
 
-    it("testGetLinksOfUserCorrectValue", (done: any) => {
-        var expectedValues = ["Nice", "Work", "Connor"];
-        var queryResult: pg.QueryResult =  {
-            command: "", rowCount: 3, rows: expectedValues, oid: 0
-        };
-        poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
-                                      TypeMoq.It.isValue(["validAuthParam"])))
-                                      .returns(() => Promise.resolve(queryResult));
-        postgresConnector = new PostgresConnector(poolMock.object);
-        postgresConnector.getLinksOfUser("validAuthParam")
-        .then(links => {
-            for(var i = 0; i < links.length; i++) {
-                expect(links[i]).toBe(expectedValues[i]);
+        afterEach(() => {
+            poolMock = null;
+        });
+
+        it("testGetLinksOfUserNotNull", (done: any) => {
+            var queryResult: pg.QueryResult =  {
+                command: "", rowCount: 3, rows: ["Nice", "Work", "Connor"], oid: 0
             }
-            done();
-        }).catch(() => {
-            fail();
-            done();
+            poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
+                                        TypeMoq.It.isValue(["validAuthParam"])))
+                                        .returns(() => Promise.resolve(queryResult));
+            postgresConnector = new PostgresConnector(poolMock.object);
+            postgresConnector.getLinksOfUser("validAuthParam")
+            .then(links => {
+                expect(links).not.toBeNull();
+                done();
+            }).catch(() => {
+                fail();
+                done();
+            });
+        });
+
+        it("testGetLinksOfUserCorrectValue", (done: any) => {
+            var expectedValues = ["Nice", "Work", "Connor"];
+            var queryResult: pg.QueryResult =  {
+                command: "", rowCount: 3, rows: expectedValues, oid: 0
+            };
+            poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
+                                        TypeMoq.It.isValue(["validAuthParam"])))
+                                        .returns(() => Promise.resolve(queryResult));
+            postgresConnector = new PostgresConnector(poolMock.object);
+            postgresConnector.getLinksOfUser("validAuthParam")
+            .then(links => {
+                for(var i = 0; i < links.length; i++) {
+                    expect(links[i]).toBe(expectedValues[i]);
+                }
+                done();
+            }).catch(() => {
+                fail();
+                done();
+            });
+        });
+
+        it("testGetAllLinksInvalidAuth", (done: any) => {
+            var errorResult: PostgresError = {
+                error: "User is not defined",
+                detail: "1"
+            }
+            poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
+                                        TypeMoq.It.isValue(["invalidAuthParam"])))
+                                        .returns(() => Promise.reject(errorResult));
+            postgresConnector = new PostgresConnector(poolMock.object);
+            postgresConnector.getLinksOfUser("invalidAuthParam")
+            .then(links => {
+                fail()
+                done();
+            }).catch((err: PostgresError) => {
+                expect(err).not.toBeNull();
+                expect(err.error).toBe(errorResult.error);
+                expect(err.detail).toBe(errorResult.detail);
+                done();
+            });
         });
     });
 
-    it("testGetAllLinksInvalidAuth", (done: any) => {
-        var errorResult: PostgresError = {
-            error: "User is not defined",
-            detail: "1"
-        }
-        poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT * FROM get_links_of_user(u_auth := $1)"),
-                                      TypeMoq.It.isValue(["invalidAuthParam"])))
-                                      .returns(() => Promise.reject(errorResult));
-        postgresConnector = new PostgresConnector(poolMock.object);
-        postgresConnector.getLinksOfUser("invalidAuthParam")
-        .then(links => {
-            fail()
-            done();
-        }).catch((err: PostgresError) => {
-            expect(err).not.toBeNull();
-            expect(err.error).toBe(errorResult.error);
-            expect(err.detail).toBe(errorResult.detail);
-            done();
+    describe("createLink()", () => {
+
+        var poolMock: TypeMoq.IMock<pg.Pool>;
+        var postgresConnector: PostgresConnector;
+        
+        beforeEach(() => {
+            poolMock = TypeMoq.Mock.ofType(pg.Pool);
         });
-    })
+
+        afterEach(() => {
+            poolMock = null;
+        });
+
+        it("testCreateLinkSuccessCase", (done: any) => {
+            var queryResult: pg.QueryResult = {
+                oid: NaN, command: "SELECT", rowCount: 0, rows: []
+            }
+            poolMock.setup(x => x.query(TypeMoq.It.isValue("SELECT create_link(link_name := $1, link_url := $2, u_auth := $3)"),
+                                        TypeMoq.It.isValue(["validLinkName", "http://validLink.url", "validAuthParam"])))
+                                        .returns(() => Promise.resolve(queryResult));
+            postgresConnector = new PostgresConnector(poolMock.object);
+            postgresConnector.createLink("validLinkName", "http://validLink.url", "validAuthParam")
+            .then(result => {
+                expect(result).toBe(queryResult);
+                done();
+            }).catch(err => {
+                fail();
+                done();
+            });
+        });
+    });
 });
